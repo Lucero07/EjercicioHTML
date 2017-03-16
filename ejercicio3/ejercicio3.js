b49cf715a1a02199c592504ca7d287b707326926
Lucero <Lucero>
2017-03-16 10:37:57 -0600

    ejerciciosHTML-completo

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,9 +1,7 @@
-function Fahrenheit(numero1) {
-	// TO DO: sumar numero1 y numero2
-    return ((numero1 - 32) *5/9 );
+function fahrenheit(numero1) {
+  return ((numero1 *5/9)+ 32);
 };
 
-function Celsius(numero1) {
-	// TO DO: sumar numero1 y numero2
-    return ((numero1 *5/9)+ 32);
+function celsius(numero1) {
+  return ((numero1 - 32) *5/9 );
 };
--- ejercicio3/enlace.js
+++ ejercicio3/enlace.js
@@ -1,15 +1,25 @@
 +function() {
 	// Elementos
-	var Fahrenheit = document.getElementById("Fahrenheit");
-	var Celsius = document.getElementById("Celsius");
-  // función principal
-  var operar = function (operacion) {
-          var temperaturaF = document.getElementById("temperaturaF").value;
-          var temperaturaC = document.getElementById("temperaturaC").value;
-          // Manejadores de Evento
-          	temperaturaF.addEventListener("click", function () {
-                  operar(Fahrenheit);
-            temperaturaC.addEventListener("click", function () {
-                        operar(Celsius);
+	var botonFahrenheit = document.getElementById("convert-to-celsius");
+	var botonCelsius = document.getElementById("convert-to-fahrenheit");
+
+  botonFahrenheit.addEventListener("click", function (e) {
+    e.preventDefault();
+      var temperaturaF = document.getElementById("temperaturaF").value;
+    var resultado =  celsius(temperaturaF);
+    document.getElementById("temperaturaC").value = parseInt( resultado);
   });
-  }();
+  botonCelsius.addEventListener("click", function (e) {
+      e.preventDefault();
+    var temperaturaC = document.getElementById("temperaturaC").value;
+  var resultado =  fahrenheit(temperaturaC);
+  document.getElementById("temperaturaF").value = parseInt( resultado);
+  });
+  // función principal
+  // var operar = function (operacion) {
+  //         var temperaturaF = document.getElementById("temperaturaF").value;
+  //         var temperaturaC = document.getElementById("temperaturaC").value;
+  //         // Manejadores de Evento
+  //
+  // };
+}();
